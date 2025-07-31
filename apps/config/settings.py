d62fb12252fbe79c7c2eb0b1c4ae5a6c52b13ab9
denverprogrammer <denverprogrammer@gmail.com>
2025-07-30 22:24:50 -0600

    Fixed api error in react pages..

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -153,11 +153,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Additional static files directories
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles" / "react",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
